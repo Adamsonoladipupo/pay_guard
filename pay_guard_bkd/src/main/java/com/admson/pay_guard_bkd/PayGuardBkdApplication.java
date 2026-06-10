ecec6e96af6d526d583cfd9ac1bd97955561facb
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-06-10 02:19:59 +0100

    Auto-save 2026-06-10 02:19:59

---
+++ pay_guard_bkd/src/main/java/com/admson/pay_guard_bkd/PayGuardBkdApplication.java
@@ -0,0 +1,13 @@
+package com.admson.pay_guard_bkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayGuardBkdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayGuardBkdApplication.class, args);
+	}
+
+}
---
+++ pay_guard_bkd/src/test/java/com/admson/pay_guard_bkd/PayGuardBkdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admson.pay_guard_bkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayGuardBkdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
