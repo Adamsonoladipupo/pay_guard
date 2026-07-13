052f9573009aec7f72d5aa1f1ccb06616b3ab61a
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-13 22:17:56 +0100

    Auto-save 2026-07-13 22:17:56

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/BaseEntity.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/BaseEntity.java
@@ -1,10 +1,23 @@
 package com.pay_guard.pay_guard_bkd.data.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public abstract class BaseEntity {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(nullable = true, updatable = true)
     private UUID Id;
+
+    @CreatedDate
+    @
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
