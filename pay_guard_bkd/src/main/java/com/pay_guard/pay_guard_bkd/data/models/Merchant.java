2cca2d438649cb843786aa027eaed80e64913b2a
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-14 06:34:39 +0100

    Auto-save 2026-07-14 06:34:39

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/Merchant.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/Merchant.java
@@ -1,8 +1,17 @@
 package com.pay_guard.pay_guard_bkd.data.models;
 
+import jakarta.persistence.Column;
+
 public class Merchant extends BaseEntity {
 
+    @Column(nullable = false, unique = true, length = 50)
     private String merchantId;
+
+    @Column(nullable = false, length = 150)
     private String merchantName;
+
     
+    private String merchantCategory;
+    private MerchantStatus status = MerchantStatus.ACTIVE;
+    private List<Transaction> transactions = new ArrayList<>();
 }
