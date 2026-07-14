912ef8bebe4d8a22c69a0023654aa9ea5bfbad61
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-14 06:26:26 +0100

    Auto-save 2026-07-14 06:26:26

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/Admin.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/Admin.java
@@ -1,10 +1,26 @@
 package com.pay_guard.pay_guard_bkd.data.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
 public class Admin extends BaseEntity {
+    @Column(nullable = false, length = 100)
     private String firstName;
+
+    @Column(nullable = false, length = 100)
     private String lastName;
+
+    @Column(nullable = false, unique = true, length = 150)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserRole role;
+
+    @Column(nullable = false)
     private boolean enabled = true;
 }
