1ad3098c358eedee02bd3a15bc6e471198f7753d
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-14 06:32:35 +0100

    Auto-save 2026-07-14 06:32:35

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/Merchant.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/Merchant.java
@@ -1,5 +1,8 @@
 package com.pay_guard.pay_guard_bkd.data.models;
 
-public class Merchant {
+public class Merchant extends BaseEntity {
+
+    private String merchantId;
+    private String merchantName;
     
 }
