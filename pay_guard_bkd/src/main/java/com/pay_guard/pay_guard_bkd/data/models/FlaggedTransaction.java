b5e210737798e53820eb473e29666cb55dd1cfed
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-14 07:25:55 +0100

    Auto-save 2026-07-14 07:25:55

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/FlaggedTransaction.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/FlaggedTransaction.java
@@ -1,4 +1,18 @@
 package com.pay_guard.pay_guard_bkd.data.models;
 
-public class FlaggedTransaction {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "flagged_transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+public class FlaggedTransaction extends BaseEntity{
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transaction_id", nullable = false, unique = true)
+    private Transaction transaction;
 }
