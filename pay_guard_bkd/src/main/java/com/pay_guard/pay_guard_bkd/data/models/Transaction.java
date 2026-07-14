eb3841318ee5592c108875daaeacc59e8ee50d04
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-14 07:15:43 +0100

    Auto-save 2026-07-14 07:15:43

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/Transaction.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/Transaction.java
@@ -38,4 +38,7 @@ public class Transaction extends BaseEntity{
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "merchant_id", nullable = false)
     private Merchant merchant;
+
+    @Column(nullable = false)
+    private Boolean flagged = false;
 }
