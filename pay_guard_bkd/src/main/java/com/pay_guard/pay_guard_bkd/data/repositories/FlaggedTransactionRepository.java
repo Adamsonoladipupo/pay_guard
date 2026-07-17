a12e35c9296c4b8312b2f61f84ff4805e14c7d89
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-17 10:26:24 +0100

    Auto-save 2026-07-17 10:26:24

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/repositories/FlaggedTransactionRepository.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/repositories/FlaggedTransactionRepository.java
@@ -1,4 +1,15 @@
 package com.pay_guard.pay_guard_bkd.data.repositories;
 
-public interface FlaggedTransactionRepository {
+import com.pay_guard.pay_guard_bkd.data.models.FlaggedTransaction;
+import com.pay_guard.pay_guard_bkd.data.models.InvestigationStatus;
+import com.pay_guard.pay_guard_bkd.data.models.Severity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface FlaggedTransactionRepository extends JpaRepository <FlaggedTransaction, UUID> {
+    List<FlaggedTransaction> findBySeverity(Severity severity);
+    List<FlaggedTransaction> findByInvestigationStatus(InvestigationStatus status);
+    List<FlaggedTransaction> findByReviewedFalse();
 }
