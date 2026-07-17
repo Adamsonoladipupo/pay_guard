f012d3f6edaa29d4e042a644af615e7efb7e26a1
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-17 10:24:20 +0100

    Auto-save 2026-07-17 10:24:20

---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/repositories/FlaggedTransactionRepository.java
@@ -0,0 +1,4 @@
+package com.pay_guard.pay_guard_bkd.data.repositories;
+
+public interface FlaggedTransactionRepository {
+}
---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/repositories/TransactionRepository.java
@@ -0,0 +1,24 @@
+package com.pay_guard.pay_guard_bkd.data.repositories;
+
+import com.pay_guard.pay_guard_bkd.data.models.Transaction;
+import com.pay_guard.pay_guard_bkd.data.models.TransactionStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository <Transaction, UUID> {
+    List<Transaction> findByStatus(TransactionStatus status);
+
+    List<Transaction> findByIpAddress(String ipAddress);
+
+    List<Transaction> findByCardHash(String cardHash);
+
+    List<Transaction> findByMerchantMerchantId(String merchantId);
+
+    List<Transaction> findByCreatedAtBetween(
+            LocalDateTime start,
+            LocalDateTime end
+    );
+}
