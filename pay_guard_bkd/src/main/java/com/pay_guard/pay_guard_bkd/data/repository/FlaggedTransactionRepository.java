9050f265a6d8a324275af7005e0f2d2bb99253c5
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 00:19:34 +0100

    Auto-save 2026-07-19 00:19:34

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/repository/FlaggedTransactionRepository.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/repository/FlaggedTransactionRepository.java
@@ -1,4 +1,4 @@
-package com.pay_guard.pay_guard_bkd.data.repositories;
+package com.pay_guard.pay_guard_bkd.data.repository;
 
 import com.pay_guard.pay_guard_bkd.data.models.FlaggedTransaction;
 import com.pay_guard.pay_guard_bkd.data.models.emuns.InvestigationStatus;
--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/strategy/imp/RateLimitStrategy.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/strategy/imp/RateLimitStrategy.java
@@ -1,14 +1,40 @@
 package com.pay_guard.pay_guard_bkd.strategy.imp;
 
+import com.pay_guard.pay_guard_bkd.data.models.emuns.FraudRule;
+import com.pay_guard.pay_guard_bkd.data.models.emuns.Severity;
 import com.pay_guard.pay_guard_bkd.dtos.requests.TransactionRequest;
 import com.pay_guard.pay_guard_bkd.strategy.FraudCheckResult;
 import com.pay_guard.pay_guard_bkd.strategy.FraudDetectionStrategy;
+import com.pay_guard.pay_guard_bkd.data.repository.TransactionRepository;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class RateLimitStrategy implements FraudDetectionStrategy {
+    private final TransactionRepository repository;
+
+    public RateLimitStrategy(TransactionRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public FraudCheckResult check(TransactionRequest request) {
+        long count = repository.countByIpAddressAndCreatedAtAfter(
+                request.ipAddress(),
+                LocalDateTime.now().minusMinutes(1)
+        );
+
+        if (count > 5) {
+
+            return new FraudCheckResult(
+                    true,
+                    FraudRule.RATE_LIMIT,
+                    Severity.CRITICAL,
+                    "More than five requests detected from the same IP."
+            );
+        }
+        
         return null;
     }
 }
