4732c35d8e80de9f404d2b090f7b8e391e3080bf
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 00:54:17 +0100

    Auto-save 2026-07-19 00:54:17

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/FraudDetectionService.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/FraudDetectionService.java
@@ -1,8 +1,35 @@
 package com.pay_guard.pay_guard_bkd.services;
 
+import com.pay_guard.pay_guard_bkd.dtos.requests.TransactionRequest;
+import com.pay_guard.pay_guard_bkd.factory.FraudStrategyFactory;
+import com.pay_guard.pay_guard_bkd.strategy.FraudCheckResult;
+import com.pay_guard.pay_guard_bkd.strategy.FraudDetectionStrategy;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FraudDetectionService {
-    
+    private final FraudStrategyFactory factory;
+
+    public FraudDetectionService(FraudStrategyFactory factory) {
+        this.factory = factory;
+    }
+
+    public List<FraudCheckResult> analyze(TransactionRequest request) {
+
+        List<FraudCheckResult> results = new ArrayList<>();
+
+        for (FraudDetectionStrategy strategy : factory.getStrategies()) {
+
+            FraudCheckResult result = strategy.check(request);
+
+            if (result.fraudDetected()) {
+                results.add(result);
+            }
+        }
+
+        return results;
+    }
 }
