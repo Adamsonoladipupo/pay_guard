54b2af4cd792863ac49372c23536a80e6b1b7219
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-18 23:35:50 +0100

    Auto-save 2026-07-18 23:35:50

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/dtos/responses/DashboardResponse.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/dtos/responses/DashboardResponse.java
@@ -1,5 +1,10 @@
 package com.pay_guard.pay_guard_bkd.dtos.responses;
 
 public record DashboardResponse(
-        
+        long totalTransactions,
+        long approvedTransactions,
+        long flaggedTransactions,
+        long declinedTransactions,
+        long totalMerchants,
+        long totalAdmins
 ) {}
