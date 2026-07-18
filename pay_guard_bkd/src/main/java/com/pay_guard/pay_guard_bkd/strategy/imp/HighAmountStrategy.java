ae8435806627fe4e98565bf7cc0d3ff81d20562b
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 00:15:23 +0100

    Auto-save 2026-07-19 00:15:23

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/strategy/imp/HighAmountStrategy.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/strategy/imp/HighAmountStrategy.java
@@ -1,15 +1,38 @@
 package com.pay_guard.pay_guard_bkd.strategy.imp;
 
+import com.pay_guard.pay_guard_bkd.data.models.emuns.FraudRule;
+import com.pay_guard.pay_guard_bkd.data.models.emuns.Severity;
 import com.pay_guard.pay_guard_bkd.dtos.requests.TransactionRequest;
 import com.pay_guard.pay_guard_bkd.strategy.FraudCheckResult;
 import com.pay_guard.pay_guard_bkd.strategy.FraudDetectionStrategy;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
 public class HighAmountStrategy implements FraudDetectionStrategy {
 
+    private static final BigDecimal LIMIT = new BigDecimal("500000");
+
     @Override
     public FraudCheckResult check(TransactionRequest request) {
-        return null;
+        if (request.amount().compareTo(LIMIT) > 0) {
+
+            return new FraudCheckResult(
+                    true,
+                    FraudRule.HIGH_AMOUNT,
+                    Severity.HIGH,
+                    "Transaction amount exceeds ₦500,000."
+            );
+        }
+
+        return new FraudCheckResult(
+                false,
+                null,
+                null,
+                null
+        );
     }
+
+
 }
