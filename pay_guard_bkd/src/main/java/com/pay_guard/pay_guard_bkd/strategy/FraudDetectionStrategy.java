7b247568b3a7c08c04e8cfd0b7ae3a3fc24a1b3e
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 00:09:09 +0100

    Auto-save 2026-07-19 00:09:09

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/strategy/FraudDetectionStrategy.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/strategy/FraudDetectionStrategy.java
@@ -1,7 +1,10 @@
 package com.pay_guard.pay_guard_bkd.strategy;
 
 import com.pay_guard.pay_guard_bkd.data.models.Transaction;
+import com.pay_guard.pay_guard_bkd.dtos.requests.TransactionRequest;
 
 public interface FraudDetectionStrategy {
-    FraudCheckResult check(Transaction transaction);
+//    FraudCheckResult check(Transaction transaction);
+    FraudCheckResult check(TransactionRequest request);
 }
+
