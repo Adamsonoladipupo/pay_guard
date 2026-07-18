43e897e21bf33b522298ebc8757dcc262a6ed930
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-18 23:03:46 +0100

    Auto-save 2026-07-18 23:03:46

---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/dtos/requests/TransactionRequest.java
@@ -0,0 +1,29 @@
+package com.pay_guard.pay_guard_bkd.dtos.requests;
+
+import com.pay_guard.pay_guard_bkd.data.models.TransactionType;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.math.BigDecimal;
+
+public record TransactionRequest(
+        @NotBlank(message = "Card number is required")
+        String cardNumber,
+
+        @NotNull(message = "Amount is required")
+        @Positive
+        BigDecimal amount,
+
+        @NotBlank(message = "Merchant ID is required")
+        String merchantId,
+
+        @NotBlank(message = "IP Address is required")
+        String ipAddress,
+
+        @NotBlank(message = "Currency is required")
+        String currency,
+
+        TransactionType transactionType,
+
+        String deviceId
+) {
+}
