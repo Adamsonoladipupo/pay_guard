5d4eb538ab63c2da28cce9e51a90731e04db8028
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 00:35:49 +0100

    Auto-save 2026-07-19 00:35:49

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/strategy/imp/MerchantStrategy.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/strategy/imp/MerchantStrategy.java
@@ -5,8 +5,42 @@ import com.pay_guard.pay_guard_bkd.strategy.FraudCheckResult;
 import com.pay_guard.pay_guard_bkd.strategy.FraudDetectionStrategy;
 
 public class MerchantStrategy implements FraudDetectionStrategy {
+
+    private final MerchantRepository merchantRepository;
+
+    public MerchantStrategy(MerchantRepository merchantRepository) {
+        this.merchantRepository = merchantRepository;
+    }
+
     @Override
     public FraudCheckResult check(TransactionRequest request) {
+
+        Merchant merchant = merchantRepository
+                .findByMerchantId(request.merchantId())
+                .orElse(null);
+
+        if (merchant == null) {
+
+            return new FraudCheckResult(
+                    true,
+                    FraudRule.UNKNOWN_MERCHANT,
+                    Severity.CRITICAL,
+                    "Merchant does not exist."
+            );
+        }
+
+        if (merchant.getStatus() != MerchantStatus.ACTIVE) {
+
+            return new FraudCheckResult(
+                    true,
+                    FraudRule.BLACKLISTED_MERCHANT,
+                    Severity.CRITICAL,
+                    "Merchant is not active."
+            );
+        }
+    @Override
+    public FraudCheckResult check(TransactionRequest request) {
+
         return null;
     }
 }
