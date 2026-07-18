5fbb5ac715bea6b125112905ca42b77d68811b47
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 00:52:12 +0100

    Auto-save 2026-07-19 00:52:12

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/factory/FraudStrategyFactory.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/factory/FraudStrategyFactory.java
@@ -1,18 +1,17 @@
 package com.pay_guard.pay_guard_bkd.factory;
 
 import com.pay_guard.pay_guard_bkd.strategy.FraudDetectionStrategy;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class FraudStrategyFactory {
     private final List<FraudDetectionStrategy> strategies;
-
     public FraudStrategyFactory(
             List<FraudDetectionStrategy> strategies) {
-
         this.strategies = strategies;
     }
-
     public List<FraudDetectionStrategy> getStrategies() {
         return strategies;
     }
---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/FraudDetectionService.java
@@ -0,0 +1,8 @@
+package com.pay_guard.pay_guard_bkd.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FraudDetectionService {
+    
+}
