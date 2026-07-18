35db5216c188cf72eeef9ed940acfa6c313413c0
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-18 23:48:39 +0100

    Auto-save 2026-07-18 23:48:39

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/mappers/FlaggedTransactionMapper.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/mappers/FlaggedTransactionMapper.java
@@ -1,5 +1,11 @@
 package com.pay_guard.pay_guard_bkd.mappers;
 
+import com.pay_guard.pay_guard_bkd.data.models.FlaggedTransaction;
+import com.pay_guard.pay_guard_bkd.dtos.responses.FlaggedTransactionResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface FlaggedTransactionMapper {
     @Mapping(target = "transactionId", source = "transaction.id")
     FlaggedTransactionResponse toResponse(
