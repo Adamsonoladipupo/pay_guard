7f211936e4381856ded5c96f4d8173039ccf3bda
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 08:02:21 +0100

    Auto-save 2026-07-19 08:02:21

---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.pay_guard.pay_guard_bkd.exception;
+
+public class BusinessException extends RuntimeException {
+  public BusinessException(String message) {
+    super(message);
+  }
+}
