3419270577c16e9aaff455000f4b989c4cef5b3e
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 08:19:00 +0100

    Auto-save 2026-07-19 08:19:00

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/emuns/FraudRule.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/data/models/emuns/FraudRule.java
@@ -3,8 +3,6 @@ package com.pay_guard.pay_guard_bkd.data.models.emuns;
 public enum FraudRule {
     RATE_LIMIT,
     HIGH_AMOUNT,
-    BLACKLISTED_MERCHANT,
     VELOCITY_ATTACK,
     SUSPICIOUS_IP,
-    DEVICE_MISMATCH
 }
