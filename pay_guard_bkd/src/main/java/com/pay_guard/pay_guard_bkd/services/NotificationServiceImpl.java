0b6370f7442217c7fcee6b52ebc1ea4a900136ee
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 21:13:00 +0100

    Auto-save 2026-07-19 21:13:00

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/NotificationServiceImpl.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/NotificationServiceImpl.java
@@ -1,7 +1,25 @@
 package com.pay_guard.pay_guard_bkd.services;
 
+import com.pay_guard.pay_guard_bkd.data.models.FlaggedTransaction;
+import com.pay_guard.pay_guard_bkd.observer.TransactionObserver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService{
+    private final List<TransactionObserver> observers;
 
+    @Override
+    public void notifyObservers(
+            FlaggedTransaction flaggedTransaction
+    ) {
+
+        observers.forEach(observer ->
+                observer.update(flaggedTransaction)
+        );
+
+    }
 }
