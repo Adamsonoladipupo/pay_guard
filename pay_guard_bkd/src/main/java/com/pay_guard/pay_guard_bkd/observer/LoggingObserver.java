f15474156a43fbce6850acab6671f1a0f03282ea
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 21:02:39 +0100

    Auto-save 2026-07-19 21:02:39

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/observer/LoggingObserver.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/observer/LoggingObserver.java
@@ -1,7 +1,12 @@
 package com.pay_guard.pay_guard_bkd.observer;
 
 import com.pay_guard.pay_guard_bkd.data.models.FlaggedTransaction;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+
+@Slf4j
+@Component
 public class LoggingObserver implements TransactionObserver{
     @Override
     public void update(FlaggedTransaction flaggedTransaction) {
