d3c86fd24a595d471cadf421bb40ce313f1db94e
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 08:39:20 +0100

    Auto-save 2026-07-19 08:39:20

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/listener/AuditEventListener.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/listener/AuditEventListener.java
@@ -1,5 +1,18 @@
 package com.pay_guard.pay_guard_bkd.listener;
 
+import com.pay_guard.pay_guard_bkd.event.TransactionProcessedEvent;
+import org.springframework.context.event.EventListener;
+
 public class AuditEventListener {
-    
+    @EventListener
+    public void onTransactionProcessed(
+            TransactionProcessedEvent event
+    ) {
+
+        System.out.println(
+                "Audit Log : "
+                        + event.getTransaction().getId()
+        );
+
+    }
 }
