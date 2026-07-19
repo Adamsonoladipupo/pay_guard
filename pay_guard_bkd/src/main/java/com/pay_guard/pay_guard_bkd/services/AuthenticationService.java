3cdbc6c7d65f0ac27476b482879e98930670b248
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 14:44:52 +0100

    Auto-save 2026-07-19 14:44:52

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/AuthenticationService.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/AuthenticationService.java
@@ -1,4 +1,22 @@
 package com.pay_guard.pay_guard_bkd.services;
 
-public class AuthenticationService {
+import com.pay_guard.pay_guard_bkd.data.models.Admin;
+import com.pay_guard.pay_guard_bkd.dtos.requests.LoginRequest;
+import com.pay_guard.pay_guard_bkd.dtos.requests.RegisterAdminRequest;
+import com.pay_guard.pay_guard_bkd.dtos.responses.LoginResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+
+public interface AuthenticationService {
+    RegisterResponse registerAdmin(RegisterAdminRequest request);
+
+    LoginResponse login(LoginRequest request);
+
+    Admin getAdminById(UUID id);
+
+    Admin getAdminByEmail(String email);
+
+    boolean emailExists(String email);
 }
