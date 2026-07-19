8cd1ff4329fdd0aa41a0c981a82bfd2dadffc376
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 22:31:48 +0100

    Auto-save 2026-07-19 22:31:48

---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/controllers/AdminController.java
@@ -0,0 +1,8 @@
+package com.pay_guard.pay_guard_bkd.controllers;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+    
+}
--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/controllers/MerchantController.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/controllers/MerchantController.java
@@ -1,7 +1,50 @@
 package com.pay_guard.pay_guard_bkd.controllers;
 
+import com.pay_guard.pay_guard_bkd.dtos.requests.MerchantRequest;
+import com.pay_guard.pay_guard_bkd.dtos.responses.MerchantResponse;
+import com.pay_guard.pay_guard_bkd.services.MerchantService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/merchants")
 @RequiredArgsConstructor
 public class MerchantController {
+    private final MerchantService merchantService;
+
+    @PostMapping
+    public ResponseEntity<MerchantResponse> createMerchant(
+            @Valid @RequestBody MerchantRequest request
+    ) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        merchantService.createMerchant(request)
+                );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MerchantResponse>> getMerchants() {
+
+        return ResponseEntity.ok(
+                merchantService.getMerchants()
+        );
+    }
+
+    @GetMapping("/{merchantId}")
+    public ResponseEntity<MerchantResponse> getMerchant(
+            @PathVariable UUID merchantId
+    ) {
+
+        return ResponseEntity.ok(
+                merchantService.getMerchant(merchantId)
+        );
+    }
 }
