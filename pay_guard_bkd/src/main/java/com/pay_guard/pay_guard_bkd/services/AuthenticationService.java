2137e13aa48a4d47bc21ea0ecedbd09b650961be
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 14:46:57 +0100

    Auto-save 2026-07-19 14:46:57

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/AuthenticationService.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/AuthenticationService.java
@@ -4,19 +4,16 @@ import com.pay_guard.pay_guard_bkd.data.models.Admin;
 import com.pay_guard.pay_guard_bkd.dtos.requests.LoginRequest;
 import com.pay_guard.pay_guard_bkd.dtos.requests.RegisterAdminRequest;
 import com.pay_guard.pay_guard_bkd.dtos.responses.LoginResponse;
+import com.pay_guard.pay_guard_bkd.dtos.responses.RegisterAdminResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 
 public interface AuthenticationService {
-    RegisterResponse registerAdmin(RegisterAdminRequest request);
-
+    RegisterAdminResponse registerAdmin(RegisterAdminRequest request);
     LoginResponse login(LoginRequest request);
-
     Admin getAdminById(UUID id);
-
     Admin getAdminByEmail(String email);
-
     boolean emailExists(String email);
 }
---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/AuthenticationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.pay_guard.pay_guard_bkd.services;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AuthenticationServiceImpl {
+    
+}
