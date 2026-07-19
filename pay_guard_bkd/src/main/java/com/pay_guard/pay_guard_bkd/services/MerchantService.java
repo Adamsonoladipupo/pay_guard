396a1681a8f5c1bf5e8c1e56147697cac592c660
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 08:12:54 +0100

    Auto-save 2026-07-19 08:12:54

---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/MerchantService.java
@@ -0,0 +1,28 @@
+package com.pay_guard.pay_guard_bkd.services;
+
+import com.pay_guard.pay_guard_bkd.data.models.Merchant;
+import com.pay_guard.pay_guard_bkd.data.models.emuns.MerchantStatus;
+import com.pay_guard.pay_guard_bkd.data.repository.MerchantRepository;
+import com.pay_guard.pay_guard_bkd.exception.MerchantInactiveException;
+import com.pay_guard.pay_guard_bkd.exception.MerchantNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MerchantService {
+    private final MerchantRepository repository;
+
+    public MerchantService(MerchantRepository repository) {
+        this.repository = repository;
+    }
+    public Merchant findActiveMerchant(String merchantId) {
+
+        Merchant merchant = repository.findByMerchantId(merchantId)
+                .orElseThrow(() ->
+                        new MerchantNotFoundException(merchantId));
+
+        if (merchant.getStatus() != MerchantStatus.ACTIVE) {
+            throw new MerchantInactiveException(merchantId);
+        }
+        return merchant;
+    }
+}
