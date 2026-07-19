478641b47b144783bc7682ae962d65df42b2da7f
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 01:22:51 +0100

    Auto-save 2026-07-19 01:22:51

---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/configuration/FraudProperties.java
@@ -0,0 +1,123 @@
+package com.pay_guard.pay_guard_bkd.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "payguard.fraud")
+public class FraudProperties {
+    private Thresholds thresholds = new Thresholds();
+
+    private HighAmount highAmount = new HighAmount();
+
+    private RateLimit rateLimit = new RateLimit();
+
+    private Velocity velocity = new Velocity();
+
+    public Thresholds getThresholds() {
+        return thresholds;
+    }
+
+    public HighAmount getHighAmount() {
+        return highAmount;
+    }
+
+    public RateLimit getRateLimit() {
+        return rateLimit;
+    }
+
+    public Velocity getVelocity() {
+        return velocity;
+    }
+
+    public static class Thresholds {
+
+        private int approve;
+
+        private int review;
+
+        private int decline;
+
+        public int getApprove() {
+            return approve;
+        }
+
+        public void setApprove(int approve) {
+            this.approve = approve;
+        }
+
+        public int getReview() {
+            return review;
+        }
+
+        public void setReview(int review) {
+            this.review = review;
+        }
+
+        public int getDecline() {
+            return decline;
+        }
+
+        public void setDecline(int decline) {
+            this.decline = decline;
+        }
+    }
+
+    public static class HighAmount {
+
+        private BigDecimal limit;
+
+        public BigDecimal getLimit() {
+            return limit;
+        }
+
+        public void setLimit(BigDecimal limit) {
+            this.limit = limit;
+        }
+    }
+
+    public static class RateLimit {
+
+        private int maxRequests;
+
+        private int windowMinutes;
+
+        public int getMaxRequests() {
+            return maxRequests;
+        }
+
+        public void setMaxRequests(int maxRequests) {
+            this.maxRequests = maxRequests;
+        }
+
+        public int getWindowMinutes() {
+            return windowMinutes;
+        }
+
+        public void setWindowMinutes(int windowMinutes) {
+            this.windowMinutes = windowMinutes;
+        }
+    }
+
+    public static class Velocity {
+
+        private int maxTransactions;
+
+        private int windowMinutes;
+
+        public int getMaxTransactions() {
+            return maxTransactions;
+        }
+
+        public void setMaxTransactions(int maxTransactions) {
+            this.maxTransactions = maxTransactions;
+        }
+
+        public int getWindowMinutes() {
+            return windowMinutes;
+        }
+
+        public void setWindowMinutes(int windowMinutes) {
+            this.windowMinutes = windowMinutes;
+        }
+    }
+
+}
