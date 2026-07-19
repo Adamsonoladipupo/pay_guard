0bb9d6effbf00c529631a870a6031f679ee5389d
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 08:37:16 +0100

    Auto-save 2026-07-19 08:37:16

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/event/TransactionProcessedEvent.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/event/TransactionProcessedEvent.java
@@ -1,4 +1,15 @@
 package com.pay_guard.pay_guard_bkd.event;
 
+import com.pay_guard.pay_guard_bkd.data.models.Transaction;
+
 public class TransactionProcessedEvent {
+    private final Transaction transaction;
+
+    public TransactionProcessedEvent(Transaction transaction) {
+        this.transaction = transaction;
+    }
+
+    public Transaction getTransaction() {
+        return transaction;
+    }
 }
