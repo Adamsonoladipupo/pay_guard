61909dffa3a62e475d0b808899bd9c3308018c06
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 22:29:41 +0100

    Auto-save 2026-07-19 22:29:41

---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/controllers/MerchantController.java
@@ -0,0 +1,7 @@
+package com.pay_guard.pay_guard_bkd.controllers;
+
+@RestController
+@RequestMapping("/api/v1/merchants")
+@RequiredArgsConstructor
+public class MerchantController {
+}
--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/controllers/TransactionController.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/controllers/TransactionController.java
@@ -1,8 +1,42 @@
 package com.pay_guard.pay_guard_bkd.controllers;
 
+import com.pay_guard.pay_guard_bkd.dtos.requests.TransactionRequest;
+import com.pay_guard.pay_guard_bkd.dtos.responses.TransactionResponse;
+import com.pay_guard.pay_guard_bkd.services.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/transactions")
 @RequiredArgsConstructor
 public class TransactionController {
+    private final TransactionService transactionService;
 
+    @PostMapping
+    public ResponseEntity<TransactionResponse> processTransaction(
+            @Valid @RequestBody TransactionRequest request
+    ) {
+
+        TransactionResponse response =
+                transactionService.processTransaction(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @GetMapping("/{transactionId}")
+    public ResponseEntity<TransactionResponse> getTransaction(
+            @PathVariable UUID transactionId
+    ) {
+
+        return ResponseEntity.ok(
+                transactionService.getTransaction(transactionId)
+        );
+    }
 }
