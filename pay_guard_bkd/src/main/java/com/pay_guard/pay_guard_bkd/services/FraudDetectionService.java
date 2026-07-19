687d12a429c8f228a24801ea075f057029eedb53
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-19 01:14:42 +0100

    Auto-save 2026-07-19 01:14:42

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/FraudDetectionService.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/FraudDetectionService.java
@@ -1,7 +1,9 @@
 package com.pay_guard.pay_guard_bkd.services;
 
+import com.pay_guard.pay_guard_bkd.data.models.emuns.TransactionStatus;
 import com.pay_guard.pay_guard_bkd.dtos.requests.TransactionRequest;
 import com.pay_guard.pay_guard_bkd.factory.FraudStrategyFactory;
+import com.pay_guard.pay_guard_bkd.services.model.FraudAnalysisResult;
 import com.pay_guard.pay_guard_bkd.strategy.FraudCheckResult;
 import com.pay_guard.pay_guard_bkd.strategy.FraudDetectionStrategy;
 import org.springframework.stereotype.Service;
@@ -11,25 +13,39 @@ import java.util.List;
 
 @Service
 public class FraudDetectionService {
+
     private final FraudStrategyFactory factory;
 
     public FraudDetectionService(FraudStrategyFactory factory) {
         this.factory = factory;
     }
 
-    public List<FraudCheckResult> analyze(TransactionRequest request) {
-
+    public FraudAnalysisResult analyze(TransactionRequest request) {
         List<FraudCheckResult> results = new ArrayList<>();
-
+        int totalRiskScore = 0;
         for (FraudDetectionStrategy strategy : factory.getStrategies()) {
-
             FraudCheckResult result = strategy.check(request);
-
             if (result.fraudDetected()) {
                 results.add(result);
+                totalRiskScore += result.riskScore();
             }
         }
+        boolean fraudDetected = !results.isEmpty();
+        TransactionStatus status =
+                determineTransactionStatus(totalRiskScore);
 
-        return results;
+        return new FraudAnalysisResult(
+                fraudDetected,
+                totalRiskScore,
+                status,
+                results
+        );
+    }
+
+    private TransactionStatus determineTransactionStatus(int score) {
+        if (score >= 80) {
+            return TransactionStatus.FLAGGED;
+        }
+        return TransactionStatus.APPROVED;
     }
 }
