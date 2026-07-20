8909c8afe9345b27da936aa9902d23d98b70b25d
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-20 10:38:04 +0100

    Auto-save 2026-07-20 10:38:04

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/TransactionService.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/services/TransactionService.java
@@ -1,5 +1,12 @@
 package com.pay_guard.pay_guard_bkd.services;
 
+import com.pay_guard.pay_guard_bkd.data.models.Merchant;
+import com.pay_guard.pay_guard_bkd.dtos.requests.MerchantRequest;
+import com.pay_guard.pay_guard_bkd.dtos.responses.MerchantResponse;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface TransactionService {
-    
+
 }
