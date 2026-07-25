850c789748261317eea22c2ac25c2aba830b8835
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-25 11:04:10 +0100

    Auto-save 2026-07-25 11:04:10

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/aspect/ExceptionLoggingAspect.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/aspect/ExceptionLoggingAspect.java
@@ -1,10 +1,15 @@
 package com.pay_guard.pay_guard_bkd.aspect;
 
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Aspect
 @Component
@@ -13,5 +18,30 @@ public class ExceptionLoggingAspect {
             LoggerFactory.getLogger(
                     ExceptionLoggingAspect.class
             );
-    
+    @AfterThrowing(
+            pointcut = "execution(* com.pay_guard.pay_guard_bkd.services..*(..))",
+            throwing = "exception"
+    )
+    public void logException(
+            JoinPoint joinPoint,
+            Exception exception
+    ) {
+
+        String className =
+                joinPoint.getTarget()
+                        .getClass()
+                        .getSimpleName();
+
+        String methodName =
+                joinPoint.getSignature()
+                        .getName();
+
+        log.error(
+                "{}.{}() failed.",
+                className,
+                methodName,
+                exception
+        );
+    }
+
 }
