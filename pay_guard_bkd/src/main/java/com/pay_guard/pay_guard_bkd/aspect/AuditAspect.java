629d41fa53993ae9e94a77cb69d1f5d628695298
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-25 11:22:27 +0100

    Auto-save 2026-07-25 11:22:27

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/aspect/AuditAspect.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/aspect/AuditAspect.java
@@ -1,5 +1,18 @@
 package com.pay_guard.pay_guard_bkd.aspect;
 
+import com.pay_guard.pay_guard_bkd.annotation.Audit;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
 public class AuditAspect {
+    private static final Logger log =
+            LoggerFactory.getLogger(
+                    AuditAspect.class
+            );
+    
 }
