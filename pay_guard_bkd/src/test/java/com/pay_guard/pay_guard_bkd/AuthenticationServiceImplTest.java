a3fc25154194b8a2817a3d69b991e834082ae103
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-25 13:30:04 +0100

    Auto-save 2026-07-25 13:30:04

--- pay_guard_bkd/src/test/java/com/pay_guard/pay_guard_bkd/AuthenticationServiceImplTest.java
+++ pay_guard_bkd/src/test/java/com/pay_guard/pay_guard_bkd/AuthenticationServiceImplTest.java
@@ -3,28 +3,18 @@ package com.pay_guard.pay_guard_bkd;
 import com.pay_guard.pay_guard_bkd.data.models.Admin;
 import com.pay_guard.pay_guard_bkd.data.models.emuns.UserRole;
 import com.pay_guard.pay_guard_bkd.data.repository.AdminRepository;
-import com.pay_guard.pay_guard_bkd.dtos.requests.LoginRequest;
 import com.pay_guard.pay_guard_bkd.dtos.requests.RegisterAdminRequest;
-import com.pay_guard.pay_guard_bkd.dtos.responses.LoginResponse;
 import com.pay_guard.pay_guard_bkd.dtos.responses.RegisterResponse;
 import com.pay_guard.pay_guard_bkd.exception.BusinessException;
-import com.pay_guard.pay_guard_bkd.exception.InvalidCredentialsException;
 import com.pay_guard.pay_guard_bkd.mappers.AdminMapper;
-import com.pay_guard.pay_guard_bkd.security.AdminDetails;
-import com.pay_guard.pay_guard_bkd.security.AdminDetailsService;
-import com.pay_guard.pay_guard_bkd.security.JwtService;
 import com.pay_guard.pay_guard_bkd.services.AuthenticationServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
@@ -39,20 +29,13 @@ public class AuthenticationServiceImplTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    @Mock
-    private AuthenticationManager authenticationManager;
 
     @Mock
     private AdminMapper adminMapper;
 
-    @Mock
-    private JwtService jwtService;
 
     @InjectMocks
     private AuthenticationServiceImpl service;
-
-    @InjectMocks
-    private AdminDetailsService adminDetailsService;
 
     @Test
     void shouldRegisterAdminSuccessfully() {
@@ -125,91 +108,5 @@ public class AuthenticationServiceImplTest {
         verify(adminRepository, never()).save(any());
     }
 
-    @Test
-    void shouldLoginSuccessfully() {
 
-        LoginRequest request = new LoginRequest(
-                "admin@payguard.com",
-                "password"
-        );
-
-        Admin admin = new Admin();
-
-        admin.setEmail(request.email());
-
-        UserDetails userDetails =
-                mock(UserDetails.class);
-
-        when(adminRepository.findByEmail(request.email()))
-                .thenReturn(Optional.of(admin));
-
-        when(adminDetailsService.loadUserByUsername(request.email()))
-                .thenReturn(userDetails);
-
-        when(jwtService.generateToken(userDetails))
-                .thenReturn("jwt-token");
-
-        LoginResponse response = service.login(request);
-
-        assertThat(response).isNotNull();
-
-        assertThat(response.token())
-                .isEqualTo("jwt-token");
-
-        assertThat(response.type())
-                .isEqualTo("Bearer");
-
-        verify(authenticationManager)
-                .authenticate(any(UsernamePasswordAuthenticationToken.class));
-
-        verify(jwtService)
-                .generateToken(userDetails);
-    }
-
-    @Test
-    void shouldThrowExceptionWhenAdminDoesNotExist() {
-
-        LoginRequest request = new LoginRequest(
-                "missing@payguard.com",
-                "password"
-        );
-
-        when(adminRepository.findByEmail(request.email()))
-                .thenReturn(Optional.empty());
-
-        doNothing().when(authenticationManager)
-                .authenticate(any());
-
-        assertThatThrownBy(() ->
-                service.login(request))
-                .isInstanceOf(InvalidCredentialsException.class);
-    }
-
-    @Test
-    void shouldThrowExceptionWhenPasswordIsIncorrect() {
-
-        LoginRequest request =
-                new LoginRequest(
-                        "admin@payguard.com",
-                        "wrong-password"
-                );
-
-        Admin admin = new Admin();
-
-        admin.setEmail(request.email());
-
-        admin.setPassword("encoded");
-
-        when(adminRepository.findByEmail(request.email()))
-                .thenReturn(Optional.of(admin));
-
-        when(passwordEncoder.matches(
-                request.password(),
-                admin.getPassword()
-        )).thenReturn(false);
-
-        assertThatThrownBy(() ->
-                service.login(request))
-                .isInstanceOf(BusinessException.class);
-    }
 }
