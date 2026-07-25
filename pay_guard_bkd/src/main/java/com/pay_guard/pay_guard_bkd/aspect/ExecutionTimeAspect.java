74043cb9115b8b3033fc6fb4513750dcaff1b401
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-25 09:53:19 +0100

    Auto-save 2026-07-25 09:53:19

---
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/aspect/ExecutionTimeAspect.java
@@ -0,0 +1,11 @@
+package com.pay_guard.pay_guard_bkd.aspect;
+
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ExecutionTimeAspect {
+    
+}
