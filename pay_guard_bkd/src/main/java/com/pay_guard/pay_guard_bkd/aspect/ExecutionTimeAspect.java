af4f9cffbdecda1cd3c8f7856aa8ab85b3f67d63
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-25 10:19:23 +0100

    Auto-save 2026-07-25 10:19:23

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/aspect/ExecutionTimeAspect.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/aspect/ExecutionTimeAspect.java
@@ -1,11 +1,40 @@
 package com.pay_guard.pay_guard_bkd.aspect;
 
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class ExecutionTimeAspect {
-    
+    @Around("execution(* com.pay_guard.pay_guard_bkd.services..*(..))")
+    public Object measureExecutionTime(
+            ProceedingJoinPoint joinPoint
+    ) throws Throwable {
+        long start = System.currentTimeMillis();
+        String className =
+                joinPoint.getTarget()
+                        .getClass()
+                        .getSimpleName();
+        String methodName =
+                joinPoint.getSignature()
+                        .getName();
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long executionTime =
+                    System.currentTimeMillis() - start;
+            System.out.println(
+                    className
+                            + "."
+                            + methodName
+                            + "() completed in "
+                            + executionTime
+                            + " ms"
+            );
+        }
+    }
 }
