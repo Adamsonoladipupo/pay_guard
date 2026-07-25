0204ba0bde75e1ad8d0c2896db9f4acf3228c5a9
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-07-25 14:20:50 +0100

    Auto-save 2026-07-25 14:20:50

--- pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/exception/TransactionNotFoundException.java
+++ pay_guard_bkd/src/main/java/com/pay_guard/pay_guard_bkd/exception/TransactionNotFoundException.java
@@ -2,6 +2,6 @@ package com.pay_guard.pay_guard_bkd.exception;
 
 public class TransactionNotFoundException extends BusinessException {
     public TransactionNotFoundException(String id) {
-        super("Transaction " + id + " not found.");
+        super("Transaction not found.");
     }
 }
